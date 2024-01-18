59a6aea98b7aa37c912108451076b72b8736424c
Aiden <aidenhaledev@gmail.com>
2024-01-17 23:13:32 -0500

    Add git-aware timestamps (#13)
    Extend sitemap to include home and about pages

--- pkg/monke/util.go
+++ pkg/monke/util.go
@@ -2,9 +2,16 @@ package monke
 
 import (
 	"net/url"
+	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/labstack/gommon/log"
 )
+
+var Gitdir string = "./.git"
 
 func sanitizePath(s string) string {
 	if len(s) == 0 {
@@ -33,4 +40,24 @@ func SanitizeUrl(s string) string {
 	}
 	u.Path = sanitizePath(u.Path)
 	return u.String()
+}
+
+func GitRevision() (string, error) {
+	cmd := exec.Command("git", "--git-dir", Gitdir, "rev-parse", "HEAD")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Warnf("failed to detect git revision: %+v", err)
+		return strconv.FormatInt(time.Now().Unix(), 10), err
+	}
+	return strings.Trim(string(out), "\n"), nil
+}
+
+func GitLastLogTimeISO(path string) (time.Time, error) {
+	cmd := exec.Command("git", "--git-dir", Gitdir, "log", "-1", "--pretty=%aI", "--", path)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Warnf("failed to detect git log for %s: %+v", path, err)
+		return time.Time{}, err
+	}
+	return time.Parse(time.RFC3339, strings.Trim(string(out), "\n"))
 }
--- pkg/pages/head.go
+++ pkg/pages/head.go
@@ -1,30 +1,14 @@
 package pages
 
-import (
-	"os/exec"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/labstack/gommon/log"
-)
+import "mrdmitry/blog/pkg/monke"
 
 type HeadSnippet struct {
 	GitRev string
 }
 
-func getRevision() string {
-	cmd := exec.Command("git", "rev-parse", "HEAD")
-	out, err := cmd.Output()
-	if err != nil {
-		log.Warnf("failed to detect git revision: %+v", err)
-		return strconv.FormatInt(time.Now().Unix(), 10)
-	}
-	return strings.Trim(string(out), "\n")
-}
-
 func NewHeadSnippet() HeadSnippet {
+	revision, _ := monke.GitRevision()
 	return HeadSnippet{
-		GitRev: getRevision(),
+		GitRev: revision,
 	}
 }
